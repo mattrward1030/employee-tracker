de3d57349ec28761f6b6cfed4a0194e1276be130
Matthew Ward <mattrward1030@gmail.com>
2021-04-07 16:05:56 -0400

    started inquirer prompts and then added switch cases based on user selection

--- employeeTrackerCRUD.js
+++ employeeTrackerCRUD.js
@@ -14,9 +14,60 @@ const connection = mysql.createConnection({
     database: 'employee_trackerDB',
 });
 
+const start = () => {
+    // Prompt user using Inquirer
+    // with responses make queries
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "EXIT"],
+                name: "userSelection"
+
+            }
+        ])
+        .then((answer) => {
+            switch (answer.userSelection) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+
+                case "View All Employees By Department":
+                    viewEmployeesDepartment();
+                    break;
+
+                case "View All Employees By Manager":
+                    viewEmployeesManager();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;
+
+                case "EXIT":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+
 // Connect to the DB
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    connection.end();
+    start();
 });
