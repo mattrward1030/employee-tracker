36965f14aef1d6533a25606a18f1493d7bfaaa7b
Matthew Ward <mattrward1030@gmail.com>
2021-04-08 16:36:26 -0400

    trying to figure out how to add 2 functions into this one function

--- employeeTrackerCRUD.js
+++ employeeTrackerCRUD.js
@@ -178,34 +178,53 @@ const viewEmployeesByRole = () => {
         })
 };
 
+
+
 const addEmployee = () => {
+
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "What is the employee's first name",
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "What is the employee's last name?",
+            },
+
+
+
+        ])
+        .then((answer) => {
+            console.log(answer);
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: addRole(),
+                    manager_id: managerChoice(),
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Employee added!');
+                    // re-prompt the user for if they want to make other selections
+                    start();
+                }
+            );
+        });
+
+};
+
+const managerChoice = () => {
     connection.query('SELECT * FROM manager', (err, results) => {
         console.log(results);
         inquirer
             .prompt([
-                {
-                    name: "firstName",
-                    type: "input",
-                    message: "What is the employee's first name",
-                },
-                {
-                    name: "lastName",
-                    type: "input",
-                    message: "What is the employee's last name?",
-                },
-                // {
-                //     name: "role",
-                //     type: "rawlist",
-                //     message: "What is the employee's role",
-                //     choices() {
-                //         const roleArray = [];
-                //         results.forEach(({ title }) => {
-                //             roleArray.push(title)
-                //         })
-                //         return roleArray;
-                //     }
-                // },
-
                 {
                     name: "manager",
                     type: "rawlist",
@@ -217,7 +236,6 @@ const addEmployee = () => {
                         })
                         return managerArray;
                     }
-
                 },
 
 
@@ -235,21 +253,71 @@ const addEmployee = () => {
                 connection.query(
                     'INSERT INTO employee SET ?',
                     {
-                        first_name: answer.firstName,
-                        last_name: answer.lastName,
-                        // role_id: chosenRole,
                         manager_id: chosenManager,
                     },
                     (err) => {
                         if (err) throw err;
                         console.log('Employee added!');
                         // re-prompt the user for if they want to make other selections
-                        start();
+
                     }
                 );
             });
     });
 };
+
+
+
+const addRole = () => {
+    connection.query('SELECT * FROM role', (err, results) => {
+        console.log(results);
+        inquirer
+            .prompt([
+
+                {
+                    name: "role",
+                    type: "rawlist",
+                    message: "What is the employee's role",
+                    choices() {
+                        const roleArray = [];
+                        results.forEach(({ title }) => {
+                            roleArray.push(title)
+                        })
+                        return roleArray;
+                    }
+                },
+
+
+            ])
+            .then((answer) => {
+                console.log(answer);
+                // when finished prompting, insert a new item into the db with that info
+                let chosenRole;
+                results.forEach((role) => {
+                    if (answer.role === role.title) {
+                        chosenRole = role.id;
+                        return chosenRole;
+                    }
+                });
+                connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+
+                        role_id: chosenRole,
+
+                    },
+                    (err) => {
+                        if (err) throw err;
+                        console.log('Employee added!');
+                        // re-prompt the user for if they want to make other selections
+
+                    }
+                );
+            });
+    });
+};
+
+
 const viewEmployeesDepartment = () => {
     inquirer.prompt([
         {
--- sratch.js
+++ sratch.js
@@ -1,33 +1,59 @@
-const addEmployee = () => {
+const managerChoice = () => {
     connection.query('SELECT * FROM manager', (err, results) => {
         console.log(results);
         inquirer
             .prompt([
                 {
-                    name: "firstName",
-                    type: "input",
-                    message: "What is the employee's first name",
+                    name: "manager",
+                    type: "rawlist",
+                    message: "Who is the employee's manager",
+                    choices() {
+                        const managerArray = [];
+                        results.forEach(({ name }) => {
+                            managerArray.push(name)
+                        })
+                        return managerArray;
+                    }
                 },
-                {
-                    name: "lastName",
-                    type: "input",
-                    message: "What is the employee's last name?",
-                },
-                // {
-                //     name: "role",
-                //     type: "rawlist",
-                //     message: "What is the employee's role",
-                //     choices() {
-                //         const roleArray = [];
-                //         results.forEach(({ title }) => {
-                //             roleArray.push(title)
-                //         })
-                //         return roleArray;
-                //     }
-                // },
+
+
+            ])
+            .then((answer) => {
+                console.log(answer);
+                // when finished prompting, insert a new item into the db with that info
+                let chosenManager;
+                results.forEach((manager) => {
+                    if (answer.manager === manager.name) {
+                        chosenManager = manager.id;
+                        return chosenManager;
+                    }
+                });
+                connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        manager_id: chosenManager,
+                    },
+                    (err) => {
+                        if (err) throw err;
+                        console.log('Employee added!');
+                        // re-prompt the user for if they want to make other selections
+
+                    }
+                );
+            });
+    });
+};
+
+
+
+const addRole = () => {
+    connection.query('SELECT * FROM role', (err, results) => {
+        console.log(results);
+        inquirer
+            .prompt([
 
                 {
-                    name: "manager",
+                    name: "role",
                     type: "rawlist",
                     message: "What is the employee's role",
                     choices() {
@@ -37,7 +63,6 @@ const addEmployee = () => {
                         })
                         return roleArray;
                     }
-
                 },
 
 
@@ -55,16 +80,15 @@ const addEmployee = () => {
                 connection.query(
                     'INSERT INTO employee SET ?',
                     {
-                        first_name: answer.firstName,
-                        last_name: answer.lastName,
-                        // role_id: chosenRole,
-                        manager_id: chosenManager,
+
+                        role_id: chosenRole,
+
                     },
                     (err) => {
                         if (err) throw err;
                         console.log('Employee added!');
                         // re-prompt the user for if they want to make other selections
-                        start();
+
                     }
                 );
             });
